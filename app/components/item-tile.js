29dd48fb57b4dd2ed1c7cde790805301c812c415
Aundra Miller <miller.aundra@gmail.com>
2016-04-08 15:02:44 -0700

    add star-rating helper to single-item view

--- app/components/item-tile.js
+++ app/components/item-tile.js
@@ -1,7 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  model() {
-    ratings: this.store.findAll('record', this.get('item').id);
-  }
 });
--- app/components/ratings-display.js
+++ app/components/ratings-display.js
@@ -1,4 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // sortBy: ['stars:desc'],
+  // sortedRatings: Ember.computed.sort(this.get('item.ratings'), this.sortBy)
 });
