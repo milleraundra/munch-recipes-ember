17bf9e4244a56e09fe68172a92f743ec6a986be1
Aundra Miller <miller.aundra@gmail.com>
2016-04-08 14:49:12 -0700

    add content security policy to config/env file

--- config/environment.js
+++ config/environment.js
@@ -21,6 +21,15 @@ module.exports = function(environment) {
     },
     sassOptions: {
       includePaths: ['bower_components/materialize/sass']
+    },
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self' 'unsafe-eval' https://*.firebaseio.com",
+      'font-src': "'self'",
+      'connect-src': "'self' wss://*.firebaseio.com",
+      'img-src': "'self' *",
+      'style-src': "'self' 'unsafe-inline'",
+      'frame-src': "'self' https://*.firebaseio.com"
     }
   };
 
